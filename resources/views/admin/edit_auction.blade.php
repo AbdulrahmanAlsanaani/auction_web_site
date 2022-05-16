bd71259773599f93afe5f943314cf11cfc7102f8
Fuad Alamoudi <fuad.h.alamoudi@gmail.com>
2022-05-16 17:07:09 +0300

    FrontEnd Final Page Edit Acutoin

--- resources/views/admin/edit_auction.blade.php
+++ resources/views/admin/edit_auction.blade.php
@@ -173,32 +173,149 @@
                         <div class="col-lg-2"><h5 class="my-1">صور السيارة</h5></div>
                         <div class="col-lg-10">
                             <div class="row">
-
-                                <!-- Preview -->
-                                <div class="dropzone-previews mt-3" id="file-previews"></div>
-
-                                <!-- file preview template -->
-                                <div  id="uploadPreviewTemplate">
+                                <!-- preview template -->
+                                <div class="col-lg-3 col-md-3">
                                     <div class="card mt-1 mb-0 shadow-none border">
-                                        <div class="p-2">
-                                            <div class="row">
-                                                <div class="col-auto">
-                                                    <img data-dz-thumbnail="" src="assets/images/users/avatar-2.jpg" class="avatar-lg rounded bg-light" alt="">
-                                                </div>
-                                                <div class="col ps-0">
-                                                    <a href="javascript:void(0);" class="text-muted fw-bold" data-dz-name=""></a>
-                                                    <p class="mb-0" data-dz-size=""></p>
-                                                </div>
-                                                <div class="col-auto">
-                                                    <!-- Button -->
-                                                    <a href="" class="btn btn-link btn-lg text-muted" data-dz-remove="">
-                                                        <form><i class="dripicons-cross"></i></form>
-                                                    </a>
-                                                </div>
+                                        <div class="p-1">
+                                            <div class="">
+                                                <img data-dz-thumbnail="" src="assets/images/users/avatar-2.jpg" class="avatar-lx rounded bg-light" alt="">
+                                                <form><button type="button" class="btn-close" style="position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    /* padding: 0.9375rem 1.25rem; */"></button></form>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                                <div class="col-lg-3 col-md-3">
+                                    <div class="card mt-1 mb-0 shadow-none border">
+                                        <div class="p-1">
+                                            <div class="">
+                                                <img data-dz-thumbnail="" src="assets/images/users/avatar-2.jpg" class="avatar-lx rounded bg-light" alt="">
+                                                <form><button type="button" class="btn-close" style="position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    /* padding: 0.9375rem 1.25rem; */"></button></form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-3">
+                                    <div class="card mt-1 mb-0 shadow-none border">
+                                        <div class="p-1">
+                                            <div class="">
+                                                <img data-dz-thumbnail="" src="assets/images/users/avatar-2.jpg" class="avatar-lx rounded bg-light" alt="">
+                                                <form><button type="button" class="btn-close" style="position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    /* padding: 0.9375rem 1.25rem; */"></button></form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-3">
+                                    <div class="card mt-1 mb-0 shadow-none border">
+                                        <div class="p-1">
+                                            <div class="">
+                                                <img data-dz-thumbnail="" src="assets/images/users/avatar-2.jpg" class="avatar-lx rounded bg-light" alt="">
+                                                <form><button type="button" class="btn-close" style="position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    /* padding: 0.9375rem 1.25rem; */"></button></form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-3">
+                                    <div class="card mt-1 mb-0 shadow-none border">
+                                        <div class="p-1">
+                                            <div class="">
+                                                <img data-dz-thumbnail="" src="assets/images/users/avatar-2.jpg" class="avatar-lx rounded bg-light" alt="">
+                                                <form><button type="button" class="btn-close" style="position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    /* padding: 0.9375rem 1.25rem; */"></button></form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-3">
+                                    <div class="card mt-1 mb-0 shadow-none border">
+                                        <div class="p-1">
+                                            <div class="">
+                                                <img data-dz-thumbnail="" src="assets/images/users/avatar-2.jpg" class="avatar-lx rounded bg-light" alt="">
+                                                <form><button type="button" class="btn-close" style="position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    /* padding: 0.9375rem 1.25rem; */"></button></form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-3">
+                                    <div class="card mt-1 mb-0 shadow-none border">
+                                        <div class="p-1">
+                                            <div class="">
+                                                <img data-dz-thumbnail="" src="assets/images/users/avatar-2.jpg" class="avatar-lx rounded bg-light" alt="">
+                                                <form><button type="button" class="btn-close" style="position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    /* padding: 0.9375rem 1.25rem; */"></button></form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-3">
+                                    <div class="card mt-1 mb-0 shadow-none border">
+                                        <div class="p-1">
+                                            <div class="">
+                                                <img data-dz-thumbnail="" src="assets/images/users/avatar-2.jpg" class="avatar-lx rounded bg-light" alt="">
+                                                <form><button type="button" class="btn-close" style="position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    /* padding: 0.9375rem 1.25rem; */"></button></form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-3">
+                                    <div class="card mt-1 mb-0 shadow-none border">
+                                        <div class="p-1">
+                                            <div class="">
+                                                <img data-dz-thumbnail="" src="assets/images/users/avatar-2.jpg" class="avatar-lx rounded bg-light" alt="">
+                                                <form><button type="button" class="btn-close" style="position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    /* padding: 0.9375rem 1.25rem; */"></button></form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-3">
+                                    <div class="card mt-1 mb-0 shadow-none border">
+                                        <div class="p-1">
+                                            <div class="">
+                                                <img data-dz-thumbnail="" src="assets/images/users/avatar-2.jpg" class="avatar-lx rounded bg-light" alt="">
+                                                <form><button type="button" class="btn-close" style="position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    /* padding: 0.9375rem 1.25rem; */"></button></form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
 
                                 <!-- end file preview template -->
                             </div>
